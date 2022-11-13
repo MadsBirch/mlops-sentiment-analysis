1daf6bc41c8a6ef7590c7e453724d242a913cfed
Mads Birch Sørensen <sdam94@gmail.com>
2022-11-13 14:31:29 +0100

    update to tests.yml

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,15 +3,6 @@ import torch
 import pytest
 import torch.utils.data as data
 
-_TEST_ROOT = os.path.dirname(__file__)  # root of test folder
-_PROJECT_ROOT = os.path.dirname(_TEST_ROOT)  # root of project
-_PATH_DATA = os.path.join(_PROJECT_ROOT, "data")  # root of data
-
-#print(_TEST_ROOT)
-#print(_PROJECT_ROOT)
-#print(_PATH_DATA)
-
-
 """
 We will now test the dataset splits are of correct length.
 
