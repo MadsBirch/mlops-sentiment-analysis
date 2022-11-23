5c9978fdf8e5b1628305e5fb6d852f64462a2957
MirenLurBarquin <96773840+MirenLurBarquin@users.noreply.github.com>
2022-11-23 12:36:45 +0100

    flake8 fixes

--- src/data/AmazonReviewData.py
+++ src/data/AmazonReviewData.py
@@ -3,31 +3,31 @@ import torch.utils.data as data
 
 
 class AmazonReviewsDataset(data.Dataset):
-  def __init__(self, df, tokenizer, max_len):
-    self.df = df
-    self.max_len = max_len
-    self.tokenizer = tokenizer
+    def __init__(self, df, tokenizer, max_len):
+        self.df = df
+        self.max_len = max_len
+        self.tokenizer = tokenizer
 
-  def __len__(self):
-    return len(self.df)
+    def __len__(self):
+        return len(self.df)
 
-  def __getitem__(self, idx):
-    review = self.df['review'].iloc[idx]
-    sentiment = torch.tensor(int(self.df['sentiment'].iloc[idx]), dtype = torch.long)
+    def __getitem__(self, idx):
+        review = self.df['review'].iloc[idx]
+        sentiment = torch.tensor(int(self.df['sentiment'].iloc[idx]), dtype = torch.long)
 
-    encoding = self.tokenizer.encode_plus(
-        review,
-        max_length=self.max_len,
-        padding='max_length',
-        return_tensors='pt',
-        truncation=True,
-        add_special_tokens=True,
-        return_attention_mask=True
-    )
+        encoding = self.tokenizer.encode_plus(
+            review,
+            max_length=self.max_len,
+            padding='max_length',
+            return_tensors='pt',
+            truncation=True,
+            add_special_tokens=True,
+            return_attention_mask=True
+        )
 
-    return {
-        'review': review,
-        'input_ids': encoding.input_ids.flatten(),
-        'attention_mask': encoding.attention_mask.flatten(),
-        'labels': sentiment
-    }
+        return {
+            'review': review,
+            'input_ids': encoding.input_ids.flatten(),
+            'attention_mask': encoding.attention_mask.flatten(),
+            'labels': sentiment
+        }
