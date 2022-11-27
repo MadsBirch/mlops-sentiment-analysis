1944770a846bcd29553380604fd943003ac6403b
MirenLurBarquin <96773840+MirenLurBarquin@users.noreply.github.com>
2022-11-27 15:20:48 +0100

    0 workers updated

--- train.sh
+++ train.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 BUCKET_NAME=mlops-data-bucket
-JOB_NAME=bucket_1_epoch_0_workers_v7
-
-#gcloud ai-platform jobs submit training ${JOB_NAME} \
-#    --region=europe-west1 \
-#    --master-image-uri=gcr.io/mlops-sentiment-analysis-gcp/mlops-sentiment-analysis-image:latest \
-#    --scale-tier=CUSTOM \
-#    --master-machine-type=n1-standard-8 \
-#    --master-accelerator=type=nvidia-tesla-k80,count=1 \
+JOB_NAME=bucket_1epoch_0workers_v1
 
 gcloud ai-platform jobs submit training ${JOB_NAME} \
-    --region=europe-west3 \
-    --master-image-uri=gcr.io/mlops-sentiment-analysis-gcp/mlops-sentiment-analysis-image:latest \
+    --region=europe-west1 \
+    --master-image-uri=gcr.io/mlops-sentiment-analysis/mlops-sentiment-analysis-image:latest \
+    --scale-tier=CUSTOM \
+    --master-machine-type=n1-standard-8 \
+    --master-accelerator=type=nvidia-tesla-k80,count=1 \
