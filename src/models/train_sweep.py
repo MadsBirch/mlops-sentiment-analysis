82029e4cdb8fc5ab449623c1d65e67823e3fbb72
Mads Birch Sørensen <sdam94@gmail.com>
2022-12-04 15:55:09 +0100

    flake8

--- src/models/train_sweep.py
+++ src/models/train_sweep.py
@@ -1,15 +1,5 @@
-import random
-
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
 import yaml
-from torch.utils.data import DataLoader
-from tqdm import tqdm
 
 import wandb
 from src.models.train_utils import (
